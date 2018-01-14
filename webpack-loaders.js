7084313b011881e3fb4b8a1e7ced290816af7cc7
“Tea <teateearu@gmail.com>
2018-01-14 20:10:15 +0100

    Refactor

--- webpack-loaders.js
+++ webpack-loaders.js
@@ -1,22 +1,22 @@
 const webpack = require('webpack');
 
 exports.devServer = function(options) {
-    return {
-        devServer:{
-            historyApiFallback: true,
-            hot: true,
-            inline: true,
-            stats: 'errors-only',
-            host: options.host,
-            port: options.port,
-            contentBase: './dist',
-        },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin({
-                multistep: true
-            })
-        ]
-    };
+	return {
+		devServer:{
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			stats: 'errors-only',
+			host: options.host,
+			port: options.port,
+			contentBase: './dist',
+		},
+		plugins: [
+			new webpack.HotModuleReplacementPlugin({
+				multistep: true
+			})
+		]
+	};
 }
 
 exports.setupCSS = function(paths) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+"use strict";
+
 const path = require('path');
 const merge = require('webpack-merge');
 const validate = require('webpack-validator');
+
 const parts = require('./webpack-loaders');
 
 const PATHS = {
@@ -10,21 +13,21 @@ const PATHS = {
 };
 
 const common = {
-    entry: {
-        app: PATHS.src
-    },
-    output: {
-        path: PATHS.dist,
-        filename: 'bundle.js'
-    },
-    module: {
+	entry: {
+		app: PATHS.src
+	},
+	output: {
+		path: PATHS.dist,
+		filename: 'bundle.js'
+	},
+	module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel'
     }]
   },
-    resolve: {
+	resolve: {
     extensions: ['', '.js', '.jsx']
   }
 };
@@ -32,27 +35,27 @@ const common = {
 let config;
 
 switch(process.env.npm_lifecycle_event) {
-    case 'server':
-        config = merge(
-            common,
-            {
-              devtool: 'source-map'
-            },
-            parts.setupCSS(PATHS.css)
-        );
-        break;
-    default:
-        config = merge(
-            common,
-            {
-              devtool: 'eval-source-map'
-            },
-            parts.setupCSS(PATHS.css),
-            parts.devServer({
-                host: process.env.host,
-                port: 3000
-            })
-        );
+	case 'server':
+		config = merge(
+		  common,
+      {
+        devtool: 'source-map'
+      },
+      parts.setupCSS(PATHS.css)
+	  );
+		break;
+	default:
+		config = merge(
+			common,
+			{
+	      devtool: 'eval-source-map'
+	    },
+	    parts.setupCSS(PATHS.css),
+			parts.devServer({
+				host: process.env.host,
+				port: 3000
+			})
+		);
 }
 
 module.exports = validate(config);
